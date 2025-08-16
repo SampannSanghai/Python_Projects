d0b83765ede03fda56c375d21a04b1b2f2e2ef7a
SampannSanghai <sanghaisampann@gmail.com>
2025-08-16 10:22:52 +0530

    Added a madlibs game

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#In this project we will create a madlibs game
+#Madlibs is a story game where useer will fill in the blanks with words
+#We take input from user
+
+adj1 = input("Enter Adjective 1 : ")
+adj2 = input("Enter Adjective 2 : ")
+adj3 = input("Enter Adjective 3 : ")
+
+n1 = input("Enter a Noun (person,place,thing) : ")
+
+v1 = input("Enter Verb 1 (ing format) : ")
+
+print(f"\nToday i went to a {adj1} zoo")
+print(f"There i saw a {n1}")
+print(f"{n1} was very {adj2} and was {v1} when i saw it")
+print(f"I felt {adj3}")
