7c9010a14c076aab02dc236bd08df25817040054
SampannSanghai <sanghaisampann@gmail.com>
2025-08-16 10:19:38 +0530

    added area_rectangle calculator

---
+++ area_rectangle_calc.py
@@ -0,0 +1,6 @@
+length = float(input("Enter Length in cm : "))
+width = float(input("Enter width in cm : "))
+
+area = (length*width)
+
+print("Area of rectangle with these measurements is : ",area," cm^2")
