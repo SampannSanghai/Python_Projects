b9b305cdf5d282200b5efbd8694ea1c08818eded
SampannSanghai <sanghaisampann@gmail.com>
2025-08-16 10:25:07 +0530

    added basic function that takes input from user

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your name : ") #this commands takes users name
+age = int(input("Enter your age : "))  #this command takes users age in integer fomat
+
+print(" \nHello ",name," !!") #this command displays name
+print("Your age is ",age, " years") #this command displays age
